0d2092ace652284772c8cdb96c02c512bc498480
Emanuele <emanuelegorga@gmail.com>
2018-12-26 16:01:17 +0000

    User Story 3: As an air traffic controller To ensure safety I want to prevent takeoff when weather is stormy.

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,5 +5,12 @@ class Airport
   end
 
   def take_off(plane)
+    raise 'Cannot take off plane: weather is stormy.' if stormy?
+  end
+
+  private
+
+  def stormy?
+    rand(1..6) > 4
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,6 +2,7 @@ require 'airport'
 
 RSpec.describe Airport do
   subject(:airport) { described_class.new }
+  let(:plane) { double :plane }
 
   it 'instructs a plane to land' do
     expect(airport).to respond_to(:land).with(1).argument
@@ -9,6 +10,11 @@ RSpec.describe Airport do
 
   it 'instructs a plane to take off' do
     expect(airport).to respond_to(:take_off).with(1).argument
+  end
+
+  it 'raises an error if asked to take off a plane when weather is stormy' do
+    allow(airport).to receive(:stormy?).and_return true
+    expect{ airport.take_off(plane) }.to raise_error 'Cannot take off plane: weather is stormy.'
   end
 
 end
