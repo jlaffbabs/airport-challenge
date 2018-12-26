37de983b58f70bf514565fb3b2a21f51cd8ef48b
Emanuele <emanuelegorga@gmail.com>
2018-12-26 16:56:25 +0000

    User Story 5: As an air traffic controller To ensure safety I want to prevent landing when the airport is full.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,15 @@
 require_relative 'plane'
 
 class Airport
+  def initialize(capacity)
+    @capacity = capacity
+    @hangar = []
+  end
+
   def land(plane)
+    raise 'Cannot land plane: airport full.' if full?
     raise 'Cannot land plane: weather is stormy.' if stormy?
+    @hangar << plane
   end
 
   def take_off(plane)
@@ -13,5 +20,9 @@ class Airport
 
   def stormy?
     rand(1..6) > 4
+  end
+
+  def full?
+    @hangar.length >= @capacity
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,24 +1,52 @@
 require 'airport'
 
 RSpec.describe Airport do
-  subject(:airport) { described_class.new }
+  subject(:airport) { described_class.new(20) }
   let(:plane) { double :plane }
 
-  it 'instructs a plane to land' do
-    expect(airport).to respond_to(:land).with(1).argument
+  describe '#land' do
+    context 'when not stormy' do
+      before do
+        allow(airport).to receive(:stormy?).and_return false
+      end
+
+      it 'instructs a plane to land' do
+        expect(airport).to respond_to(:land).with(1).argument
+      end
+
+      context 'when full' do
+        it 'raise an error' do
+          20.times do
+            airport.land(plane)
+          end
+          expect{ airport.land(plane) }.to raise_error 'Cannot land plane: airport full.'
+        end
+      end
+    end
+
+    context 'when stormy' do
+      it 'raises an error' do
+        allow(airport).to receive(:stormy?).and_return true
+        expect{ airport.land(plane) }.to raise_error 'Cannot land plane: weather is stormy.'
+      end
+    end
   end
 
-  it 'instructs a plane to take off' do
-    expect(airport).to respond_to(:take_off).with(1).argument
-  end
+  describe '#take_off' do
+    context 'when not stormy' do
+      it 'instructs a plane to take off' do
+        expect(airport).to respond_to(:take_off).with(1).argument
+      end
+    end
 
-  it 'raises an error if asked to take off a plane when weather is stormy' do
-    allow(airport).to receive(:stormy?).and_return true
-    expect{ airport.take_off(plane) }.to raise_error 'Cannot take off plane: weather is stormy.'
-  end
-
-  it 'raises an error if asked to land a plane when weather is stormy' do
-    allow(airport).to receive(:stormy?).and_return true
-    expect{ airport.land(plane) }.to raise_error 'Cannot land plane: weather is stormy.'
+    context 'when stormy' do
+      before do
+        allow(airport).to receive(:stormy?).and_return true
+      end
+      
+      it 'raises an error' do
+        expect{ airport.take_off(plane) }.to raise_error 'Cannot take off plane: weather is stormy.'
+      end
+    end
   end
 end
